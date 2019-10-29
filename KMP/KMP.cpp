4f3323d23bad6c944109f7f86e3d9f643586aba1
chihyanglee <chihyanglee@gmail.com>
2019-10-29 21:26:59 +0800

    kmp search once

--- KMP/KMP.cpp
+++ KMP/KMP.cpp
@@ -7,7 +7,7 @@
 #include <string>
 #include <vector>
 using namespace std;
-void getPrefixTable(const string& p) {
+vector<int> getPrefixTable(const string& p) {
     int length = int(p.size());
     vector<int> prefixTable;
     prefixTable.push_back(-1);
@@ -15,7 +15,7 @@ void getPrefixTable(const string& p) {
     int previousIndex, currentIndex = 1;
     while (currentIndex < length - 1) {
         previousIndex = currentIndex - 1;
-        int previousTableValue = prefixTable[previousIndex];
+        int previousTableValue = prefixTable[previousIndex + 1];
         if (p[currentIndex] == p[previousTableValue]) {
             prefixTable.push_back(previousTableValue + 1);
             currentIndex += 1;
@@ -24,12 +24,35 @@ void getPrefixTable(const string& p) {
         currentIndex += 1;
         prefixTable.push_back(0);
     }
-    for (auto s : prefixTable) {
-        cout << s << " ";
+    return prefixTable;
+}
+
+void kmp_search(const string& s, const string& p) {
+    auto prefixTable = getPrefixTable(p);
+    int pLength = int(p.size());
+    int sLength = int(s.size());
+    int i = 0, j = 0;
+    while (true) {
+        if (i + pLength > sLength) {
+            cout << "search over" << endl;
+            break;
+        }
+        if (s[i] == p[j]) {
+            i++, j++;
+            if (j == pLength) {
+                cout << "pattern found! " << endl
+                    << "index at " << i - pLength << endl;
+            }
+            continue;
+        }
+        j = prefixTable[j];
+        if (s[i] != p[j]) {
+            i++, j++;
+        }
     }
 }
 int main() {
-    string T = "abaacababcac";
+    string T = "acbbbababbabbabcbaba";
     string p = "ababbab";
-    getPrefixTable(p);
+    kmp_search(T, p);
 }
